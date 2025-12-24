31b035bfc08f064d69b5ad6e4489a617a30dca84
ryu <ryuunosuke4180719@icloud.com>
2025-12-25 03:52:57 +0900

    京葉の編集&確認

--- app5.js
+++ app5.js
@@ -1,3 +1,4 @@
+"use strict";
 const express = require("express");
 const app = express();
 
@@ -49,6 +50,7 @@ app.get("/keiyo2/delete/:number", (req, res) => {
   res.redirect('/keiyo2' );
 });
 
+//Create
 app.post("/keiyo2", (req, res) => {
   // 本来ならここにDBとのやり取りが入る
   const id = station2.length + 1;
@@ -66,7 +68,7 @@ app.post("/keiyo2", (req, res) => {
 app.get("/keiyo2/edit/:number", (req, res) => {
   // 本来ならここにDBとのやり取りが入る
   const number = req.params.number;
-  const detail = station2[ number ];
+  const detail = station2[ number - 1 ];
   res.render('keiyo2_edit', {id: number, data: detail} );
 });
 
@@ -92,23 +94,37 @@ app.get("/keiyo", (req, res) => {
 //   res.render('db2', { data: station });
 // });
 
-app.get("/samortan",(req,res) =>{
-  res.render('samoran',{deta:samoran});
+app.get("/samoran",(req,res) =>{
+  res.render('samoran',{data:samochara});
 });
 
-app.get("samoran/:number",(req,res)=>{
+app.get("/samoran/:number",(req,res)=>{
   const number = req.params.number;
-  const detail = samoran[number];
-  res.render('samoran_detail',{deta:detail});
+  const detail = samochara[number];
+  res.render('samoran_detail',{data:detail});
 });
 
 let samochara = [
-  { id:1, name:"東京駅"},
-  { id:2, name:"舞浜駅"},
-  { id:3, name:"新習志野駅"},
-  { id:4, name:"幕張豊砂駅"},
-  { id:5, name:"海浜幕張駅"},
-  { id:6, name:"新浦安駅"},
+  { id:1, name:"シャケ",kind:'雑魚シャケ',attack:'近づいて殴ってくる',deth:'インクショットを与える',power:'なし'},
+  { id:2, name:"コジャケ",kind:'雑魚シャケ',attack:'近づいて殴ってくる',deth:'インクショットを与える',power:'なし'},
+  { id:3, name:"ドスコイ",kind:'雑魚シャケ',attack:'近づいて殴ってくる',deth:'インクショットを与える',power:'なし'},
+  { id:4, name:"バクダン",kind:'オオモノシャケ',attack:'頭部から爆弾を生成し標的に投げてくる',deth:'生成されている爆弾にインクショットを与え爆発させる',power:'装甲貫通武器で本体を直接攻撃する'},
+  { id:5, name:"テッパン",kind:'オオモノシャケ',attack:'標的に突進してくる',deth:'インクショットを与え装甲を破壊し後ろから攻撃',power:'装甲貫通武器で本体を直接攻撃する'},
+  { id:6, name:"ヘビ",kind:'オオモノシャケ',attack:'標的に突進してくる',deth:'一番後ろにいる操縦士にインクショットを与える',power:'なし'},
+  { id:7, name:"モグラ",kind:'オオモノシャケ',attack:'標的を追いかけ地面から捕食する',deth:'捕食のタイミングでボムを投げて食べさせる',power:'出てきたタイミングで直接攻撃する'},
+  { id:8, name:"タワー",kind:'オオモノシャケ',attack:'標的を遠くからハイパープレッサー(貫通遠距離砲)によって攻撃',deth:'積まれている鍋を全て飛ばす',power:'なし'},
+  { id:9, name:"コウモリ",kind:'オオモノシャケ',attack:'標的にアメフラシの弾を打ちアメフラシを生成',deth:'弾をインクショットで跳ね返す',power:'装甲貫通武器で本体を攻撃する'},
+  { id:10, name:"カタパッド",kind:'オオモノシャケ',attack:'標的に計八発のミサイルを発射',deth:'ミサイル発射口が開いたらそれぞれにボムを投げ込み破壊する',power:'貫通武器で真ん中の操縦士を直接攻撃する'},
+  { id:11, name:"ハシラ",kind:'オオモノシャケ',attack:'柱を設置し上からインクを撒き散らす',deth:'うえの撒き散らすシャケを全員倒す',power:'なし'},
+  { id:12, name:"ダイバー",kind:'オオモノシャケ',attack:'標的の周りにエリアを生成しその中にいる標的をすべて倒す',deth:'エリアをインクショットによって塗り替えし装甲が剥がれたところを攻撃する',power:'飛び込む準備をしている間に直接攻撃する'},
+  { id:13, name:"テッキュウ",kind:'オオモノシャケ',attack:'海辺に設置した発射台から鉄球を投げ，鉄球の着地と同時に2回波を発生させる',deth:'本体を直接攻撃する',power:'なし'},
+  { id:14, name:"ナベブタ",kind:'オオモノシャケ',attack:'自身の下からシャケを生成するのと同時に真下に標的がいたら上から潰す',deth:'上から頭を出している操縦士を攻撃する',power:'なし'},
+  { id:15, name:"グリル",kind:'オオモノシャケ',attack:'標的に突進してくる',deth:'後ろの弱点を攻撃したあとさらに出てきた弱点を攻撃し続け倒す',power:'装甲貫通武器によって直接攻撃する'},
+  { id:16, name:"キンシャケ",kind:'オオモノシャケ',attack:'近づいて殴ってくる',deth:'インクショットを与える',power:'なし'},
+  { id:17, name:"ヨコヅナ",kind:'オカシラシャケ',attack:'標的に突進，スーパーチャクチをして攻撃',deth:'インクショットを与える',power:'ジョーがいる場合はジョーに何度も食べさせる'},
+  { id:18, name:"タツ",kind:'オカシラシャケ',attack:'口から超巨大爆弾を生成し投げてくる',deth:'生成されている爆弾にインクショットを与え爆発させる',power:'ジョーがいる場合はジョーに爆弾を爆発させる，もしくは本体を直接攻撃する'},
+  { id:19, name:"ジョー",kind:'オカシラシャケ',attack:'標的を追いかけ味方諸共すべて地面から捕食する',deth:'後ろにあるたんこぶ（弱点）を攻撃する',power:'本体を直接攻撃する'},
+  { id:20, name:"オカシラ連合",kind:'オカシラシャケ',attack:'ヨコヅナ，タツ，ジョーが同時に出てきてそれぞれの攻撃を行う',deth:'それぞれのオカシラシャケをそれぞれの方法で討伐する',power:'ジョーの攻撃でヨコヅナとタツの爆弾を巻き込むことで二つを倒し，ジョーは弱点を攻撃し続けて三体目も倒す'},
 ];
 
 
--- tasks.md
+++ tasks.md
@@ -4,15 +4,15 @@
 
 
 - [ ] 開発者向けドキュメントの作業
-  - [ ] 対象とするシステムの決定 :thinking:
+  - [x] 対象とするシステムの決定 :thinking:
   - [ ] データ構造の決定 :thinking:
   - [ ] ページ構造の検討 :thinking:
   - [ ] ページ遷移の検討 :thinking:
   - [ ] HTTPメソッドとリソース名の決定
   - [ ] ページ遷移図の作成 :writing_hand:
   - [ ] ページ構造の決定
-  - [ ] ドキュメントの構成の検討 :thinking:
-  - [ ] 概要 :writing_hand:
+  - [x] ドキュメントの構成の検討 :thinking:
+  - [x] 概要 :writing_hand:
   - [ ] HTTPメソッドとリソース名一覧 :writing_hand:
   - [ ] データ構造 :writing_hand:
   - [ ] リソース名ごとの機能の詳細 :writing_hand:
